2288e4294340cbc647004c5996df4369c9f61277
Jupna21 <jupna21@yahoo.com>
2018-07-10 18:58:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(int)
   
-  if int % 3 == 0 
-    puts "fizz"
-  
-  if int % 5 == 0
-    puts "buzz"
-    
   if int % 5 && %3 == 0
     puts "fizzbuzz"
-end
+    
+  elsif int % 3 == 0 
+    puts "fizz"
   
+  elsif int % 5 == 0
+    puts "buzz"
+
+end
   
 end
 
